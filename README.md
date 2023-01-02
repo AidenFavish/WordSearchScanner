6ea938710b491876b029fb5ec18b2a5aee7f34d1
Aiden F <aidenfavish@gmail.com>
2023-01-02 15:24:59 -0800

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # WordSearchScanner
 
-A description of this package.
+A program that will take an input of the image of a wordsearch and the dimensions of the word search and recognize each individual character and assemble and return it as a string.
--- Tests/WordSearchScannerTests/WordSearchScannerTests.swift
+++ Tests/WordSearchScannerTests/WordSearchScannerTests.swift
@@ -6,6 +6,7 @@ final class WordSearchScannerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(WordSearchScanner().text, "Hello, World!")
+        XCTAssertEqual(WordSearchScanner().text, "Hello, Tester!")
+        
     }
 }
